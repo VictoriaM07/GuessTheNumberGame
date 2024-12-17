d105ca6af14ccd1a6a1cbadb23757ee10e270895
Decebal <Decebal@Stefans-MacBook-Pro.local>
2024-12-17 19:46:37 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Declara o variabila numar ascuns de la 1 la 100
+        // Cere de la user un numar de un infinit de ori
+        // Daca numar < numarAscuns afiseaza "Mai mult"
+        // Daca numar > numarAscuns afiseaza "Mai putin"
+        // Daca numar == numarAscuns afiseaza "You guess" si opreste bucla
+
+
+
+
+
+    }
+}
